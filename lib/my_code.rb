431bbc839d2a293e493d800ff2d84fed58b89423
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-26 22:03:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,41 @@
 # My Code here....
+def map_to_negativize(array)
+  array.size.times do |counter|
+    array[counter] *= -1 
+  end
+  array
+end
+
+def map_to_no_change(input)
+  input
+end
+
+def map_to_double(array)
+  array.size.times do |counter|
+    array[counter] *= 2 
+  end
+  array
+end
+
+def map_to_square(array)
+  array.size.times do |counter|
+    array[counter] = array[counter]**2
+  end
+  array
+end
+
+def reduce_to_total(array, start = 0)
+  output = start
+  array.size.times do |counter|
+    output += array[counter]
+  end
+  output
+end
+
+def reduce_to_all_true(array)
+  array.size.times do |counter|
+    array[counter] *= -1 
+  end
+  array
+end
+  
